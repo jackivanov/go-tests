c22445b2b1faf945569e140122c4ff80040ad8fc
Jack Ivanov <e601809@gmail.com>
2019-02-04 21:46:12 +0100

    exercism.io: Space Age

--- exercism.io/go/space-age/space_age.go
+++ exercism.io/go/space-age/space_age.go
@@ -1,7 +1,24 @@
 package space
 
+// Planet is planet's name string
 type Planet string
 
+// YearOnEarth a year on Earh in seconds
+const YearOnEarth float64 = 31557600
+
+// Age returns Earth-years old age (float64) for given planet (Planet) and age in seconds (float64)
 func Age(age float64, planet Planet) float64 {
-  return 12.1
+	// AgeOnPlanets a map of planets and Earth-years orbital period
+	AgeOnPlanets := map[Planet]float64{
+		"Earth":   1,
+		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
+	}
+
+	return age / (YearOnEarth * AgeOnPlanets[planet])
 }
